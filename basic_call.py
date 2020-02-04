f0fafc3840d959d30e2bc1d78b407666006d05f5
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2020-02-04 23:17:02 +0000

    List instances method

---
+++ basic_call.py
@@ -0,0 +1,22 @@
+import argparse
+import googleapiclient.discovery
+
+def list_instances(compute, project, zone):
+    result = compute.instances().list(project=project, zone=zone).execute()
+    return result['items'] if 'items' in result else None
+
+def main(project, zone='europe-west1-b'):
+    compute = googleapiclient.discovery.build('compute', 'v1')
+    instances = list_instances(compute, project, zone)
+    print(instances)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('project_id', help='Your Google Cloud project ID.')
+    parser.add_argument('zone', help='Zone to list instances in')
+
+    args = parser.parse_args()
+
+    main(args.project_id, args.zone)
